025c8571a0e93f10f53c3198d13b5e5bde0f0931
Bacale-Kevin <bacale86@gmail.com>
2023-06-10 15:55:33 +0100

    feat: configured appwrite and added logic to fetch todos and sort todos

---
+++ components/Board.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useBoardStore } from "@/store/BoardStore";
+import { useEffect } from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
+const Board = () => {
+  const [board, getBoard] = useBoardStore((state) => [state.board, state.getBoard]);
+
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
+  console.log(board);
+
+  return (
+    <h1>Hello</h1>
+    // <DragDropContext>
+    //   <Droppable droppableId="board" direction="horizontal" type="column">
+    //     {(provided) => <div className="">Rendering all the colums</div>}
+    //   </Droppable>
+    // </DragDropContext>
+  );
+};
+
+export default Board;
